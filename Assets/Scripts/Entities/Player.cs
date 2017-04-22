4dcafdae89394d875d84998faf38fe342b6dfe2c
Flan <jamioflan@hotmail.co.uk>
2017-04-22 16:19:27 +0100

    Remove duped player

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : Entity
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float maxSpeed = 10F;
+    public int XP = 0;
+    public List<Upgrade> upgrades = new List<Upgrade>();
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rigidBody.velocity = new Vector2(moveX * maxSpeed, moveY * maxSpeed);
+    }
 }
