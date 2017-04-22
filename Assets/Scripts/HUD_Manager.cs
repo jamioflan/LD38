5dc69f57e2eb422e9f7300efce04c3da2a2c3a18
rebrebrebecca <rdodson94@yahoo.co.uk>
2017-04-22 15:23:47 +0100

    HUD

--- Assets/Scripts/HUD_Manager.cs
+++ Assets/Scripts/HUD_Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD_Manager : MonoBehaviour
 {
@@ -8,19 +9,24 @@ public class HUD_Manager : MonoBehaviour
     public string playerHealthElementName;
     public string equippedWeaponIconElementName;
 
-    GUIText amountXPElement;
-    
+    GUIText amountXPText;
+    Slider playerHealthSlider;
+    Image equippedWeaponIcon;
 
 	// Use this for initialization
 	void Start ()
     {
-		amountXPElement = GameObject.Find(amountXPElementName).GetComponent<GUIText>();
-
+        // Find and store the things we'll be modifying
+		amountXPText = GameObject.Find(amountXPElementName).GetComponent<GUIText>();
+        playerHealthSlider = GameObject.Find(playerHealthElementName).GetComponent<Slider>();
+        equippedWeaponIcon = GameObject.Find(equippedWeaponIconElementName).GetComponent<Image>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        // Update player XP, health and equipped weapon icon
+        // TODO
         
     }
 }
