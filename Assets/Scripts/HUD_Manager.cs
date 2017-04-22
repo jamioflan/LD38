dca694066720d6db22ce4930d4931429079ade89
rebrebrebecca <rdodson94@yahoo.co.uk>
2017-04-22 15:18:06 +0100

    HUD stuff

---
+++ Assets/Scripts/HUD_Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD_Manager : MonoBehaviour
+{
+    public string amountXPElementName;
+    public string playerHealthElementName;
+    public string equippedWeaponIconElementName;
+
+    GUIText amountXPElement;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+		amountXPElement = GameObject.Find(amountXPElementName).GetComponent<GUIText>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
