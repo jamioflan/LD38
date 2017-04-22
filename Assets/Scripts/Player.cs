6b95707edb7620da625d58c0b036128fe93ea7df
Ricardo <richard.mathers63@gmail.com>
2017-04-22 15:29:12 +0100

    Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed = 10F;
+
+	void Start ()
+	{
+		
+	}
+
+	void FixedUpdate ()
+	{
+
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D> ();
+
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis("Vertical");
+
+		rigidBody.velocity = new Vector2(moveX * maxSpeed, moveY * maxSpeed);
+	}
+}
