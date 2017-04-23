14519941222b0e8316aaa2e58ec2b6b193f2b673
rebrebrebecca <rdodson94@yahoo.co.uk>
2017-04-23 15:46:48 +0100

    Update XP in skills page

--- Assets/Scripts/SkillTree_Manager.cs
+++ Assets/Scripts/SkillTree_Manager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillTree_Manager : MonoBehaviour
 {
+    public GameObject amountXPObject;
+
+    Text amountXPText;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        amountXPText = amountXPObject.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        amountXPText.text = Game.thePlayer.XP.ToString();
+    }
 }
