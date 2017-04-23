6c9f3ab38acdf9a64bf48940069e6ba1c96face7
Sam Adam Day <me@samadamday.com>
2017-04-23 09:52:21 +0100

    Added general utilities script

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities {
+
+    public static void shuffle<T>(T[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int r = i + (int)Random.Range(0, (n - i));
+            T t = array[r];
+            array[r] = array[i];
+            array[i] = t;
+        }
+    }
+
+}
