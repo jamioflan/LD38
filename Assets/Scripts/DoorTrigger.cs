623f27b7baae85219331fd24f9f298b62f5dc2cb
rebrebrebecca <rdodson94@yahoo.co.uk>
2017-04-23 21:23:44 +0100

    Maybe they work

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    public RoomDoor roomDoor;
+    public RoomWall roomWall;
 
 	// Use this for initialization
 	void Start ()
     {
-        roomDoor = gameObject.GetComponentInParent<RoomDoor>();
+        roomWall = gameObject.GetComponentInParent<RoomWall>();
 	}
 	
 	// Update is called once per frame
@@ -38,10 +38,49 @@ public class DoorTrigger : MonoBehaviour
 
     public void MoveThroughDoorway(Entity entity)
     {
-        if (roomDoor != null)
+        if (roomWall != null && roomWall.door != null)
         {
-            RoomDoor targetDoor = roomDoor.leadsTo;
- //           entity.transform.position = targetDoor.
+            // We're gonna get a loop here with bosses! They'll flicker between two rooms. Needs fixing.
+
+            RoomDoor targetDoor = roomWall.door.leadsTo;
+            int wallIndexThisDoorIsOn = 0;
+
+            for (int iIndex = 0; iIndex < 4; iIndex++)
+            {
+                RoomWall wall = targetDoor.parentRoomBlock.walls[iIndex];
+                if (wall != null && wall.door == targetDoor)
+                {
+                    wallIndexThisDoorIsOn = (iIndex + targetDoor.parentRoomWall.wallDoor.positionedRoom.rotation) % 4;
+                }
+            }
+
+            Vector3 offset;
+            switch (wallIndexThisDoorIsOn)
+            {
+                case 0:
+                default:
+                {
+                    offset = new Vector3(0.0f, -DungeonPiece.tilesToWorldUnitsConversion, 0.0f);
+                    break;
+                }
+                case 1:
+                {
+                    offset = new Vector3(DungeonPiece.tilesToWorldUnitsConversion, 0.0f, 0.0f);
+                    break;
+                }
+                case 2:
+                {
+                    offset = new Vector3(0.0f, DungeonPiece.tilesToWorldUnitsConversion, 0.0f);
+                    break;
+                }
+                case 3:
+                {
+                    offset = new Vector3(-DungeonPiece.tilesToWorldUnitsConversion, 0.0f, 0.0f);
+                    break;
+                }
+            }
+
+            entity.transform.position = targetDoor.parentRoomWall.wallDoor.transform.position + offset;
         }
     }
 }
